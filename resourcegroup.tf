292bdc0dac871ce21ae5d5b0a2fe50d7d7c94414
jmarti45 <jmartinez72@hotmail.es>
2024-01-19 08:26:56 +0000

    dwdddee

---
+++ resourcegroup.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "comun" {
+  name     = "rg-${var.Enterprise1}-${var.Project1}-${var.Environment}"
+  location = var.Location.name
+  tags     = var.binding_tags
+}
