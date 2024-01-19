f131523fbfe11042eb78f3dccd2515da4664fc67
jmarti45 <jmartinez72@hotmail.es>
2024-01-19 08:33:00 +0000

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "comun" {
   name     = "rg-${var.Enterprise1}-${var.Project1}-${var.Environment}"
   location = var.Location.name
-  tags     = var.binding_tags
+  #tags     = var.binding_tags
 }
