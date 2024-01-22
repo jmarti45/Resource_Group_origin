803111a62dbf844cdb234ba7d66671fc60d13b26
jmarti45 <jmartinez72@hotmail.es>
2024-01-22 12:33:08 +0000

    zzqqzqz

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "comun" {
-  name     = "rg-${var.Enterprise1}-${var.Project1}-${var.Environment}"
+  name     = "rg-${var.Enterprise1}-${var.Project1}-${var.Environment}-69"
   location = var.Location.name
   #tags     = var.binding_tags
 }
